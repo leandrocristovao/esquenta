3f75f7346747c2d87df39e0be80963d9a9e3cb15
Leandro Cristóvão <leandro.cristovao@avalara.com>
2018-01-09 02:16:14 -0200

    .

--- BOOTSTRAP/app.js
+++ BOOTSTRAP/app.js
@@ -2,12 +2,73 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
 app.use(express.static('.'));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/comanda/:id', function (req, res) {
+	var id = req.params.id;
+	var obj = {
+		ID: 1,
+		Nome: 'Caixa',
+		EmAberto: 0
+	};
+	res.setHeader('Content-Type', 'application/json');
+	res.send(obj);
+});
+
+app.get('/produto/:id', function (req, res) {
+	var id = req.params.id;
+	var obj = {
+		ID: 1,
+		Nome: 'Meu produto',
+		Valor: 4.75
+	};
+	res.setHeader('Content-Type', 'application/json');
+	res.send(obj);
+});
+
+app.get('/comandas', function (req, res) {
+	var objs = [
+		{ ID: 1, Nome: 'Caixa', EmAberto: 0 },
+		{ ID: 2, Nome: 'Consumo', EmAberto: 0 },
+		{ ID: 3, Nome: 'Preto', EmAberto: 0 },
+		{ ID: 4, Nome: 'Luciano', EmAberto: 0 },
+		{ ID: 5, Nome: 'Amarildo', EmAberto: 0 },
+		{ ID: 6, Nome: 'Deni', EmAberto: 0 },
+		{ ID: 7, Nome: 'Marquinhos', EmAberto: 0 },
+		{ ID: 8, Nome: 'Edson Capenga', EmAberto: 0 }
+	];
+	res.setHeader('Content-Type', 'application/json');
+	res.send(objs);
+});
+
+app.get('/produtos', function (req, res) {
+	var objs = [
+		{ ID: 1, Nome: 'Heineken', Valor: 4.75 },
+		{ ID: 2, Nome: 'brahma 300 ml', Valor: 4.75 },
+		{ ID: 3, Nome: 'trindent menta', Valor: 4.75 },
+		{ ID: 4, Nome: 'vinho latitude 33', Valor: 4.75 },
+		{ ID: 5, Nome: 'jurupinga', Valor: 4.75 },
+		{ ID: 6, Nome: 'almaden branco seco', Valor: 4.75 },
+		{ ID: 7, Nome: 'maracuja pingo de ouro', Valor: 4.75 },
+		{ ID: 8, Nome: 'master gold', Valor: 4.75 },
+		{ ID: 9, Nome: 'dreher', Valor: 4.75 },
+		{ ID: 10, Nome: 'magnata', Valor: 4.75 },
+		{ ID: 11, Nome: 'rayslof tradicional', Valor: 4.75 },
+		{ ID: 12, Nome: 'aklov', Valor: 4.75 },
+		{ ID: 13, Nome: 'rayslof kiwi', Valor: 4.75 },
+		{ ID: 14, Nome: 'rayslof red fruits', Valor: 4.75 },
+		{ ID: 15, Nome: 'contini bianco', Valor: 4.75 },
+		{ ID: 16, Nome: 'martini bianco', Valor: 4.75 }
+	];
+	res.setHeader('Content-Type', 'application/json');
+	res.send(objs);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('App listening on port 3000!');
 });
--- BOOTSTRAP/index.js
+++ BOOTSTRAP/index.js
@@ -1,15 +1,31 @@
 $(document).ready(function () {
 
-    //https://github.com/bassjobsen/Bootstrap-3-Typeahead
-    $.get("example_collection.json", function (data) {
-        $("#controller").typeahead({
-            source: data,
-            afterSelect: function (item) {
-                showModalQuantidade();
-                console.log(item);
-            }
-        });
-    }, 'json');
+    var controllerData = [];
+    var venda = {
+        ItemVenda: []
+    };
+    var calculo = {
+        acrescimo: 0,
+        desconto: 0,
+        valorPago: 0,
+        troco: 0,
+        total: 0
+    }
+
+    $('#controller').typeahead({
+        source: function (query, process) {
+            return $.get('/produtos', { query: query }, function (data) {
+                controllerData = data;
+                var results = _.map(data, function (d) {
+                    return d.Nome;
+                });
+                return process(results);
+            });
+        },
+        afterSelect: function (item) {
+            showModalQuantidade();
+        }
+    });
 
     $('#table').bootstrapTable({
         columns: [{
@@ -33,18 +49,68 @@ $(document).ready(function () {
 
     $('#controller').keyup(function (e) {
         if (e.which == 13) {
-            //alert($('#controller').val());
             showModalQuantidade();
         }
     });
 
-    $('#controller').on('typeahead:selected', function (e, datum) {
-        console.log(datum);
+    $('#quantidadeModalOK').click(function (e) {
+        var item = $('#controller').val();
+        var quantidade = $('#quantidadeModalQuantidade').val();
+        item = findControllerData(item)[0];
+
+        var itemVenda = {
+            Valor: item.Valor,
+            ValorTotal: item.Valor * quantidade,
+            Quantidade: quantidade,
+            Produto: item
+        };
+        venda.ItemVenda.push(itemVenda);
+
+        $('#table').bootstrapTable('append', [{
+            id: venda.ItemVenda.length,
+            produto: itemVenda.Produto.Nome,
+            valor: itemVenda.Valor,
+            quantidade: itemVenda.Quantidade,
+            valorTotal: itemVenda.ValorTotal
+        }])
+        atualizaCalculo();
+        $('#quantidadeModal').modal('toggle');
+        $('#controller').val('');
+        $('#controller').focus();
     });
 
     function showModalQuantidade() {
-        $('#quantidadeModal').modal('show');
+        $('#quantidadeModalQuantidade').val(1)
+        $('#quantidadeModal').modal({ keyboard: true });
+    };
+
+    function findControllerData(value) {
+        return $.grep(controllerData, function (n, i) {
+            return n.Nome == value;
+        });
+    };
+
+    function atualizaCalculo() {
+        calculo.total = 0;
+        venda.ItemVenda.forEach(function (obj) { calculo.total += parseFloat(obj.ValorTotal) || 0; });
+
+        $('#lblAcrescimo').text(calculo.acrescimo.toFixed(2));
+        $('#lblDesconto').text(calculo.desconto.toFixed(2));
+        $('#lblValorPago').text(calculo.valorPago.toFixed(2));
+        $('#lblTroco').text(calculo.troco.toFixed(2));
+        $('#lblTotal').text(calculo.total.toFixed(2));
     }
+
+    $('#quantidadeModal').on('shown.bs.modal', function () {
+        $('#quantidadeModalQuantidade').focus();
+        $('#quantidadeModalQuantidade').select();
+    });
+
+    $('#quantidadeModalQuantidade').keyup(function (e) {
+        if (e.which == 13) {
+            $('#quantidadeModalOK').trigger('click');
+        }
+    });
 });
 
 /**
