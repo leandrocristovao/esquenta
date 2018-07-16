b86b5b7c8a202e769aa2221413616875567aa130
Leandro Cristovao <leandrocristovao@gmail.com>
2018-07-16 12:04:28 -0300

    .

--- V2-Node/esquenta.v2/SERVER/server/controllers/dashboard.js
+++ V2-Node/esquenta.v2/SERVER/server/controllers/dashboard.js
@@ -5,23 +5,48 @@ const ItemVenda = require('../models').itemVenda
 
 module.exports = {
 
-  get(req, res) {
+  // get (req, res) {
+  //   return PeriodoVenda
+  //     .findOne({
+  //       order: [['ID', 'DESC']]
+  //     })
+  //     .then(periodo => {
+  //       ItemVenda.findAll({
+  //         limit: 10,
+  //         where: {
+  //           dataVenda: {
+  //             [Op.gte]: periodo.dataValues.dataInicial
+  //           }
+  //         }
+  //       })
+  //         .then(vendas => {
+  //           return res.status(200).send(vendas)
+  //         })
+  //     })
+  //     .then(data => {
+  //       return res.status(200).send(data)
+  //     })
+  //     .catch(error => res.status(400).send(error))
+  // }
+  get (req, res) {
     return PeriodoVenda
       .findOne({
-        order: [['ID', 'DESC']]
+        order: [
+          ['ID', 'DESC']
+        ]
       })
       .then(periodo => {
-        ItemVenda.findAll({
+        return ItemVenda.findAll({
           limit: 10,
-           where: {
-             dataVenda: {
-               [Op.gte]: periodo.dataValues.dataInicial
-             }
+          where: {
+            dataVenda: {
+              [Op.gte]: periodo.dataValues.dataInicial
+            }
           }
         })
-          .then(vendas => {
-            return res.status(200).send(vendas)
-          })
+      })
+      .then(data => {
+        return res.status(200).send(data)
       })
       .catch(error => res.status(400).send(error))
   }
